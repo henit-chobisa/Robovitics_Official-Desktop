f2ffcc4d47ddbff713c87255956cbf74448d12de
Henit Chobisa <henitchobisa0885@gmail.com>
2021-09-26 23:56:56 +0530

    Notice Design Completed

--- Backend/Config/Authentication/routes/register.js
+++ Backend/Config/Authentication/routes/register.js
@@ -20,4 +20,4 @@ router.post('/', async (req , res) => {
     }
 })
 
-module.exports = router
+module.exports = router;
--- frontend/lib/Pages/NoticePage.dart
+++ frontend/lib/Pages/NoticePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:http/http.dart' as http;
 
 class NoticePage extends StatefulWidget {
   const NoticePage({Key? key}) : super(key: key);
@@ -39,7 +40,7 @@ class _NoticePageState extends State<NoticePage> {
               height: 750.h,
               width: 450.w,
               decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
+                  border: Border.all(color: Colors.grey, width: 0.5),
                   borderRadius: BorderRadius.circular(10.r)),
               child: Padding(
                 padding: EdgeInsets.all(20.sp),
@@ -64,10 +65,43 @@ class _NoticePageState extends State<NoticePage> {
                       height: 10,
                     ),
                     Container(
-                      height: 600,
+                      height: 550,
                       width: 500,
-                      child: SfPdfViewer.network(
-                          'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf'),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.r),
+                        child: SfPdfViewer.network(
+                            'http://www.africau.edu/images/default/sample.pdf'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        NoticeButton(
+                          buttonColor: Colors.red,
+                          buttonTitle: "Raise Concent",
+                          titleColor: Colors.white,
+                          onTap: () {
+                            print("Hello red");
+                          },
+                        ),
+                        NoticeButton(
+                          buttonTitle: "Discussions",
+                          buttonColor: Colors.yellow,
+                          titleColor: Colors.black,
+                          onTap: () => print("Hello Discussions"),
+                        ),
+                        NoticeButton(
+                          buttonTitle: "Acknowledge",
+                          buttonColor: Colors.green,
+                          titleColor: Colors.white,
+                          onTap: () {
+                            print("Acknowledged");
+                          },
+                        )
+                      ],
                     )
                   ],
                 ),
@@ -77,5 +111,38 @@ class _NoticePageState extends State<NoticePage> {
         )),
       ),
     ));
+  }
+}
+
+class NoticeButton extends StatelessWidget {
+  NoticeButton(
+      {required this.buttonTitle,
+      required this.buttonColor,
+      required this.titleColor,
+      this.onTap});
+
+  final String buttonTitle;
+  final Color buttonColor;
+  final Color titleColor;
+  final onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.transparent,
+            borderRadius: BorderRadius.circular(10.r),
+            border: Border.all(color: Colors.blueAccent)),
+        height: 40.h,
+        width: 120.w,
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: TextStyle(color: Colors.white, fontSize: 15.sp),
+          ),
+        ),
+      ),
+    );
   }
 }
