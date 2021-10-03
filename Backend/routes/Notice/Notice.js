16c3a7aa3b81536285748a6c0dbb860ca908b96b
Henit Chobisa <henitchobisa0885@gmail.com>
2021-10-03 12:10:39 +0530

    Concent Routes Added

--- Backend/routes/Notice/Notice.js
+++ Backend/routes/Notice/Notice.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const router = express.Router();
 
+// Post Requests
 router.use('/createNotice', require('./Routes/createNotice'));
 router.use('/createAcknow', require("./Routes/createAcknowledgement"));
+router.use('/addConcent', require('./Routes/addConcent'));
+router.use('/addUpvote', require('./Routes/addUpvote'));
+router.use('/addDownvotes', require('./Routes/addDownvotes'));
+
+// Get Requests
 router.use('/getNotice', require('./Routes/getNotice'));
 router.use('/getAllNotices', require('./Routes/getAllNotice'));
 router.use('/getDiscussions', require('./Routes/getAllDiscussions'));
