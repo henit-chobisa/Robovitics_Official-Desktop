249cd45559d36461c233e5c9d4f8bc069d4983d7
Henit Chobisa <henitchobisa0885@gmail.com>
2021-10-06 21:49:56 +0530

    submission route added

---
+++ Backend/routes/Tasks/Routes/addAcknowledgement.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const taskSchema = require('../../../models/Tasks/Tasks');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const {taskID, userID } = req.body;
+    const task = await taskSchema.findById(taskID);
+    task.addAcknowledgement(userID);
+    await task.save();
+})
+
+module.exports = router;
--- Backend/routes/Tasks/Tasks.js
+++ Backend/routes/Tasks/Tasks.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 // Post Requests
 router.use('/addTask', require('./Routes/addTask'));
+router.use('/addAcknowledgement', require('./Routes/addAcknowledgement'));
 
 module.exports = router;
