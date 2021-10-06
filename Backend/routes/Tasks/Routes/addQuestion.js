4e40e8614c138258c9b23128d22066194f5afeb7
Henit Chobisa <henitchobisa0885@gmail.com>
2021-10-06 22:09:49 +0530

    Task Post routes completed

--- Backend/routes/Tasks/Routes/addQuestion.js
+++ Backend/routes/Tasks/Routes/addQuestion.js
@@ -3,10 +3,10 @@ const taskSchema = require('../../../models/Tasks/Tasks');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const {taskID, userID, comment, docLink} = req.body;
+    const {taskID, question, userID} = req.body;
     const task = await taskSchema.findById(taskID);
-    task.addSubmission(userID, comment, docLink);
-    task.save();
+    task.addQuestion(question, userID);
+    await task.save();
     res.json(task);
 })
 
